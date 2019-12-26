3cc2b4742f9389707d18af47a2f1cad34c82545a
Amitesh Anand <aamitesh@VANNA.COM>
2019-12-26 15:32:25 +0530

    Added more services and used @RequestParam

--- src/main/java/com/training/demorestservice/DemorestserviceApplication.java
+++ src/main/java/com/training/demorestservice/DemorestserviceApplication.java
@@ -3,11 +3,11 @@ package com.training.demorestservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemorestserviceApplication {
 
+@SpringBootApplication(scanBasePackages = {"com.training"})
+public class DemorestserviceApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemorestserviceApplication.class, args);
 	}
-
 }
